fd5b4c065de968721dd595a892cb79f7c2cdb862
Dave Strickland <daveastrickland@gmail.com>
2022-12-10 13:33:39 -0600

    buffed up auth; linting rules

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,26 +1,32 @@
-import { stringify } from 'node:querystring'
-import axios from 'axios'
+import { stringify } from 'node:querystring';
+import axios, { AxiosResponse } from 'axios';
 const userData = {
-  current: false,
-  access_token: ''
-}
+  access_token: '',
+  expires: new Date()
+};
 export default defineEventHandler(async (event) => {
-  if (!userData.current) {
+  if (userData.expires <= new Date()) {
     const data = {
       grant_type: 'password',
       client_id: 'david_strickland',
       client_secret: '8263f45014d643bfab92e89ba0c1f3fb',
       username: 'davidstrick',
       password: 'LBBapids'
+    };
+    let result: AxiosResponse<any, any>;
+    try {
+      result = await axios.post('https://retsidentityapi.raprets.com/LUBB/oauth/token',
+        stringify(data),
+        {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }
+      );
+      userData.access_token = result.data.access_token;
+      userData.expires = result.data['.expires'];
+    } catch (error) {
+      // email error or something for logging
+      console.error(error);
     }
-    const result = await axios.post('https://retsidentityapi.raprets.com/LUBB/oauth/token',
-      stringify(data),
-      {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-      }
-    )
-    userData.current = true
-    userData.access_token = result.data.access_token
   }
-  event.context.auth = { token: userData.access_token }
-})
+  event.context.auth = { token: userData.access_token };
+});
