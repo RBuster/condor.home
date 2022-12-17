7132a6633abf5f11b0691c65da92fb2aead2e8cd
DStrickland <daveastrickland@gmail.com>
2022-12-17 15:22:31 -0600

    new navigation

--- components/navBar.vue
+++ components/navBar.vue
@@ -1,8 +1,10 @@
 <template>
-  <Disclosure v-slot="{ open }" as="nav" class="bg-gray-800">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-between">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+  <Disclosure v-slot="{ open }" as="nav" class="bg-inherit">
+    <div class="mx-auto max-w-none px-2 sm:px-6 lg:px-8 nav-shadow border-b-zinc-200">
+      <div class="flex flex-wrap justify-between lg:items-center sm:justify-start">
+        <img class="mx-0 my-7 sm:m-7 sm:hidden" src="~/assets/images/logo_sm.png" alt="Condor">
+        <img class="hidden mx-0 my-7 h-16 w-fit sm:m-7 sm:block" src="~/assets/images/logo_lg.png" alt="Condor">
+        <div class="flex justify-self-end sm:hidden align-middle">
           <!-- Mobile menu button-->
           <DisclosureButton class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
             <span class="sr-only">Open main menu</span>
@@ -10,39 +12,45 @@
             <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
           </DisclosureButton>
         </div>
-        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div class="flex flex-shrink-0 items-center">
-            <img class="block h-8 w-auto lg:hidden" src="~/assets/images/logo_sm.png" alt="Condor">
-            <img class="hidden h-8 w-auto lg:block" src="~/assets/images/logo_lg.png" alt="Condor">
-          </div>
+      </div>
+    </div>
+
+    <div class="mx-auto max-w-7xl">
+      <div class="relative flex items-center justify-between">
+        <div class="w-full items-center mt-8">
           <div class="hidden sm:ml-6 sm:block">
-            <div class="flex space-x-4">
-              <a
-                v-for="item in navigation"
-                :key="item.name"
-                class="nav-item"
-                :href="item.href"
-                :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']"
-                :aria-current="item.current ? 'page' : undefined"
-              >{{ item.name }}</a>
+            <div class="flex space-x-8 items-center justify-between">
+              <div>
+                <a
+                  v-for="item in activeLinks"
+                  :key="item.name"
+                  class="nav-item"
+                  :href="item.href"
+                  :class="[item.current ? 'text-cyan-600' : 'text-black hover:text-cyan-400', 'px-3 py-2 lg:mx-4 rounded-md text-sm font-medium']"
+                  :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</a>
+              </div>
+
+              <SearchBar />
             </div>
           </div>
         </div>
       </div>
     </div>
-
+    <!-- Mobile Menu -->
     <DisclosurePanel class="sm:hidden">
       <div class="space-y-1 px-2 pt-2 pb-3">
         <DisclosureButton
-          v-for="item in navigation"
+          v-for="item in activeLinks"
           :key="item.name"
           as="a"
           :href="item.href"
-          :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']"
+          :class="[item.current ? 'text-cyan-600' : 'text-black hover:text-cyan-400', 'block px-3 py-2 rounded-md text-base font-medium']"
           :aria-current="item.current ? 'page' : undefined"
         >
           {{ item.name }}
         </DisclosureButton>
+        <SearchBar />
       </div>
     </DisclosurePanel>
   </Disclosure>
@@ -53,9 +61,11 @@ import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline';
 const route = useRoute();
 const navigation = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Blog', href: '/blog', current: false },
-  { name: 'For Sale', href: '/forSale', current: false }
+  { name: 'Home', href: '/', current: true, showInNav: true },
+  { name: 'About', href: '#', current: false, showInNav: false },
+  { name: 'Meet the Team', href: '#', current: false, showInNav: true },
+  { name: 'Blog', href: '/blog', current: false, showInNav: false },
+  { name: 'Contact', href: '#', current: false, showInNav: false }
 ];
 navigation.forEach((navItem) => {
   if (navItem.href === route.path) {
@@ -64,4 +74,16 @@ navigation.forEach((navItem) => {
     navItem.current = false;
   }
 });
+const activeLinks = navigation.filter(item => item.showInNav);
 </script>
+<style scoped lang="scss">
+.nav-item{
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.nav-shadow{
+  box-shadow: 0 0 50px 0 black;
+}
+
+</style>
